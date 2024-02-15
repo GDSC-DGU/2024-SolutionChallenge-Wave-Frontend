697986f2d2ee4b530224c24953ce5b9710ac49f4
minseong0324 <kwan0324@naver.com>
2024-02-16 04:19:59 +0900

    Feat: loginScreen UI

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -14,73 +14,83 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                  child: Column(
+      body: Stack(
+        children: [
+          // 배경 이미지
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/loginBackground.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          // 로고 이미지와 로그인 버튼을 포함한 Column
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Image.asset(
+                    "assets/images/loginScreenLogo.png",
+                    height: 99,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // 로그인 버튼을 하단에서 50px 위에 위치시키기
+          Positioned(
+            bottom: 90,
+            left: 20,
+            right: 20,
+            child: InkWell(
+              onTap: signInWithGoogle,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                elevation: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      InkWell(
-                        onTap: () {
-                          // tap
-                          signInWithGoogle();
-                        },
-                        child: Card(
-                          margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          elevation: 2,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(
-                                "assets/icons/google.svg",
-                                height: 30,
-                                width: 30,
-                              ),
-                              const SizedBox(width: 10),
-                              const Text("Sign in with Google",
-                                  style: TextStyle(fontSize: 16)),
-                            ],
-                          ),
-                        ),
-                      )
+                      SvgPicture.asset(
+                        "assets/icons/google.svg",
+                        height: 30,
+                        width: 30,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "Sign in with Google",
+                        style: TextStyle(fontSize: 16),
+                      ),
                     ],
-                  ))
-            ],
-          )),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
+
   void signInWithGoogle() async {
-    // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    print(googleUser);
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth =
-    await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
 
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
+    final OAuthCredential credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
 
-    // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance
-        .signInWithCredential(credential)
-        .then((value) {
-      print(value.user?.displayName);
-      print(value.user?.email);
-      print(value.user?.photoURL);
-    }).onError(
-          (error, stackTrace) {
-        print(error);
-      },
-    );
+    await FirebaseAuth.instance.signInWithCredential(credential).then((value) {
+      print('${value.user?.displayName}, ${value.user?.email}, ${value.user?.photoURL}');
+    }).catchError((error) {
+      print(error);
+    });
   }
 }
