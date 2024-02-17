66ec7cd686a89ca7f3f452e7bccc28b906cf5d93
puretension <rlrlfhtm5@gmail.com>
2024-02-18 00:11:08 +0900

    Chore: Search Screen Dummy data-list

---
+++ lib/country/component/dummy_card_list.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:wave/country/component/search_countries_card.dart';
+
+Widget _buildHorizontalCardListEmergency() {
+  return SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Emergency',
+            mainTitle: 'Help the People 🇺🇦',
+            subTitle: 'of Ukraine affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Emergency',
+            mainTitle: 'Help the People 🇾🇪',
+            subTitle: 'of Yemen affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Emergency',
+            mainTitle: 'Help the People 🇸🇾',
+            subTitle: 'of Syria affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        // 추가적인 카드들
+      ],
+    ),
+  );
+}
+
+Widget _buildHorizontalCardListAlert() {
+  return SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Alert',
+            mainTitle: 'Help the People 🇺🇦',
+            subTitle: 'of Ukraine affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Alert',
+            mainTitle: 'Help the People 🇾🇪',
+            subTitle: 'of Yemen affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Alert',
+            mainTitle: 'Help the People 🇸🇾',
+            subTitle: 'of Syria affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        // 추가적인 카드들
+      ],
+    ),
+  );
+}
+
+Widget _buildHorizontalCardListCaution() {
+  return SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Caution',
+            mainTitle: 'Help the People 🇺🇦',
+            subTitle: 'of Ukraine affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Caution',
+            mainTitle: 'Help the People 🇾🇪',
+            subTitle: 'of Yemen affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: SearchCountryCard(
+            category: 'Caution',
+            mainTitle: 'Help the People 🇸🇾',
+            subTitle: 'of Syria affected by war',
+            image: Image.asset(
+              'assets/images/testImg.png',
+              fit: BoxFit.cover,
+            ),
+            views: 6239,
+          ),
+        ),
+        // 추가적인 카드들
+      ],
+    ),
+  );
+}
