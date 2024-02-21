26adda7b5c620a7aae19eaeae124811ecfc39728
puretension <rlrlfhtm5@gmail.com>
2024-02-21 19:01:11 +0900

    Feat: Custom Animation Floating Nav Bar

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -2,6 +2,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:wave/common/const/colors.dart';
+import 'package:wave/country/component/custom_animation_nav_bar.dart';
 import 'package:wave/country/view/search_country_screen.dart';
 import 'package:wave/country/view/donate_countries_screen.dart';
 import 'package:wave/map/view/global_map_screen.dart';
@@ -31,69 +32,76 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    // final items = List<Widget>.generate(4, (index) {
-    //   String iconName =
-    //       'tab${index + 1}${this.index == index ? 'On' : 'Off'}.svg';
-    //   return SvgPicture.asset('assets/icons/$iconName');
-    // });
-    final items = [
-      BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/icons/tab1${index == 0 ? 'On' : 'Off'}.svg',
-        ),
-        label: '',
-      ),
-      BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/icons/tab2${index == 1 ? 'On' : 'Off'}.svg',
-        ),
-        label: '',
-      ),
-      BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/icons/tab3${index == 2 ? 'On' : 'Off'}.svg',
-        ), label: '',
-      ),
-      BottomNavigationBarItem(
-        icon: SvgPicture.asset(
-          'assets/icons/tab4${index == 3 ? 'On' : 'Off'}.svg',
-        ), label: '',
-      ),
-    ];
+    final items = List<Widget>.generate(4, (index) {
+      String iconName =
+          'tab${index + 1}${this.index == index ? 'On' : 'Off'}.svg';
+      return SvgPicture.asset('assets/icons/$iconName');
+    });
+
+    // final items = [
+    //   BottomNavigationBarItem(
+    //     icon: SvgPicture.asset(
+    //       'assets/icons/tab1${index == 0 ? 'On' : 'Off'}.svg',
+    //     ),
+    //     label: '',
+    //   ),
+    //   BottomNavigationBarItem(
+    //     icon: SvgPicture.asset(
+    //       'assets/icons/tab2${index == 1 ? 'On' : 'Off'}.svg',
+    //     ),
+    //     label: '',
+    //   ),
+    //   BottomNavigationBarItem(
+    //     icon: SvgPicture.asset(
+    //       'assets/icons/tab3${index == 2 ? 'On' : 'Off'}.svg',
+    //     ), label: '',
+    //   ),
+    //   BottomNavigationBarItem(
+    //     icon: SvgPicture.asset(
+    //       'assets/icons/tab4${index == 3 ? 'On' : 'Off'}.svg',
+    //     ), label: '',
+    //   ),
+    // ];
+
     return Scaffold(
       body: screens[index],
-      // bottomNavigationBar: CurvedNavigationBar(
-      //   key: navigationKey, // key를 할당
-      //   animationCurve: Curves.linearToEaseOut,
-      //   animationDuration: const Duration(milliseconds: 800),
-      //   backgroundColor: PRIMARY_BLUE_COLOR,
-      //   buttonBackgroundColor:
-      //       PRIMARY_BLUE_COLOR, // PRIMARY_COLOR를 적절한 색상 값으로 변경
-      //   height: 55,
-      //   index: index,
-      //   items: items,
-      //   color: Colors.black,
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: 70,
+          child: CustomAnimationNavBar(
+            key: navigationKey, // key를 할당
+            animationCurve: Curves.easeInOut,
+            animationDuration: const Duration(milliseconds: 700),
+            backgroundColor: PRIMARY_BLUE_COLOR,
+            buttonBackgroundColor: PRIMARY_BLUE_COLOR,
+            height:70,
+            index: index,
+            color: Color(0xFF242424),
+            onTap: (selectedIndex) {
+              print(selectedIndex);
+              setState(() {
+                index = selectedIndex;
+              });
+            },
+            items: items,
+          ),
+        ),
+
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: items,      //   items: items,
       //   onTap: (selectedIndex) {
       //     print(selectedIndex);
       //     setState(() {
       //       index = selectedIndex;
       //     });
       //   },
+      //   currentIndex: index,
+      //   selectedItemColor: PRIMARY_BLUE_COLOR,
+      //   unselectedItemColor: PRIMARY_BLUE_COLOR,
+      //   backgroundColor: Colors.black,
+      //   type: BottomNavigationBarType.fixed,
       // ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: items,
-        onTap: (selectedIndex) {
-          print(selectedIndex);
-          setState(() {
-            index = selectedIndex;
-          });
-        },
-        currentIndex: index,
-        selectedItemColor: PRIMARY_BLUE_COLOR,
-        unselectedItemColor: PRIMARY_BLUE_COLOR,
-        backgroundColor: Colors.black,
-        type: BottomNavigationBarType.fixed,
-      ),
     );
   }
 }
