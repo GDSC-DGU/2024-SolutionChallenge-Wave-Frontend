c0d8b2c9b2fa3cf0af2a8586099df8830f7a5600
minseong0324 <kwan0324@naver.com>
2024-02-24 07:49:18 +0900

    Design: Undo code

--- lib/country/component/country_category_button.dart
+++ lib/country/component/country_category_button.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:wave/common/const/colors.dart';
 
@@ -27,33 +26,23 @@ class CountryCategoryButton extends StatelessWidget {
         backgroundColor = Colors.grey;
     }
 
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(20),
-        bottomRight: Radius.circular(20),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
       ),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-        child: Container(
-          decoration: BoxDecoration(
-            // 블러 효과를 더 잘 보이게 하기 위해 색상의 불투명도를 조정
-            color: backgroundColor.withOpacity(0.9),
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
-            ),
-          ),
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(6, 3, 10, 3),
-            child: Text(
-              countryName,
-              style: TextStyle(
-                color: textColor,
-                fontSize: 14,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(6,3, 10, 3),
+        child: Text(
+          countryName,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
           ),
         ),
       ),
